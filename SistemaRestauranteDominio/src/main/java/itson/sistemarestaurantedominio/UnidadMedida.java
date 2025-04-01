547e80b3f4bc411b7baff19cd479aa1f2acf00aa
pedroesquer <pedro.morales252815@potros.itson.edu.mx>
2025-03-31 20:25:26 -0700

    Cargando proyecto + mappeo de ingredientes

---
+++ SistemaRestauranteDominio/src/main/java/itson/sistemarestaurantedominio/UnidadMedida.java
@@ -0,0 +1,10 @@
+
+package itson.sistemarestaurantedominio;
+
+/**
+ *
+ * @author Pedro Morales Esquer, Juan Pablo Heras Carrazco, Victoria Valenzuela Soto
+ */
+public enum UnidadMedida {
+    PIEZAS, MILILITROS, GRAMOS
+}
