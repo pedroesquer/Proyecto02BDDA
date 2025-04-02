925a99c6cb5aede0e84b9e508a3193b03f2e7578
pedroesquer <pedro.morales252815@potros.itson.edu.mx>
2025-04-01 19:10:04 -0700

    Cambios ManejadorConexiones y agregué entidades en persistence.xml

--- SistemaRestaurantePersistencia/src/main/java/itson/sistemarestaurantepersistencia/implementaciones/ManejadorConexiones.java
+++ SistemaRestaurantePersistencia/src/main/java/itson/sistemarestaurantepersistencia/implementaciones/ManejadorConexiones.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package itson.sistemarestaurantepersistencia.implementaciones;
 
 import javax.persistence.EntityManager;
@@ -14,9 +10,9 @@ import javax.persistence.Persistence;
  */
 public class ManejadorConexiones {
     public static EntityManager getEntityManager(){
-                //Solicitamos una fabrica de entity managers
+        //Solicitamos una fabrica de entity managers
         EntityManagerFactory emFactory;
-        emFactory = Persistence.createEntityManagerFactory("itson_SistemaVideojuegos_jar_1.0PU");
+        emFactory = Persistence.createEntityManagerFactory("itson_SistemaRestaurante_jar_1.0PU");
 
         //La fabrica crea un entity manager = base de datos
         EntityManager entityManager = emFactory.createEntityManager();
