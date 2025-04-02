bd532bb5a456237978bd061b9fb7455bae4aa1a4
juan-pablitoINSOF <juan.heras247054@potros.itson.edu.mx>
2025-04-01 18:55:58 -0700

    Solo agregué la pantalla de entrada

--- SistemaRestaurantePresentacion/src/main/java/itson/sistemarestaurantepresentacion/SistemaRestaurantePresentacion.java
+++ SistemaRestaurantePresentacion/src/main/java/itson/sistemarestaurantepresentacion/SistemaRestaurantePresentacion.java
@@ -11,6 +11,6 @@ package itson.sistemarestaurantepresentacion;
 public class SistemaRestaurantePresentacion {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");        
     }
 }
