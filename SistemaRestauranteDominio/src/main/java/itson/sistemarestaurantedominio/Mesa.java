a75c0796f936d736ac8a6c15f2b4bceb002a6ec5
pedroesquer <pedro.morales252815@potros.itson.edu.mx>
2025-04-01 18:17:01 -0700

    Agregué entidad mesa con su respectivo enum

---
+++ SistemaRestauranteDominio/src/main/java/itson/sistemarestaurantedominio/Mesa.java
@@ -0,0 +1,109 @@
+package itson.sistemarestaurantedominio;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Pedro Morales Esquer, Juan Pablo Heras Carrazco, Victoria Valenzuela Soto
+ */
+@Entity
+@Table(name = "mesas")
+public class Mesa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_mesa")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "estado", nullable = false)
+    private EstadoMesa estado;
+    
+    @Column(name = "numero_mesa", nullable = false)
+    private String numeroMesa;
+    
+    @OneToMany(mappedBy = "mesa")
+    private List<Comanda> comandas;
+    
+
+    public Mesa() {
+    }
+
+    public Mesa(EstadoMesa estado, String numeroMesa, List<Comanda> comandas) {
+        this.estado = estado;
+        this.numeroMesa = numeroMesa;
+        this.comandas = comandas;
+    }
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public EstadoMesa getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoMesa estado) {
+        this.estado = estado;
+    }
+
+    public String getNumeroMesa() {
+        return numeroMesa;
+    }
+
+    public void setNumeroMesa(String numeroMesa) {
+        this.numeroMesa = numeroMesa;
+    }
+
+    public List<Comanda> getComandas() {
+        return comandas;
+    }
+
+    public void setComandas(List<Comanda> comandas) {
+        this.comandas = comandas;
+    }
+
+    
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Mesa)) {
+            return false;
+        }
+        Mesa other = (Mesa) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "itson.sistemarestaurantedominio.Mesa[ id=" + id + " ]";
+    }
+
+}
