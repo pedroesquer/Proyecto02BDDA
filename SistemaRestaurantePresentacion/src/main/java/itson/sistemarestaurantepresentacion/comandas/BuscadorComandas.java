9548df77364d2efc6d4bd8b2d88e22176cc6d705
pedroesquer <pedro.morales252815@potros.itson.edu.mx>
2025-04-11 16:29:28 -0700

    Agregué pantallas del flujo de comandas
    Co-Authored-By: Juan Pablo  <199316495+juan-pablitoINSOF@users.noreply.github.com>

--- SistemaRestaurantePresentacion/src/main/java/itson/sistemarestaurantepresentacion/comandas/BuscadorComandas.java
+++ SistemaRestaurantePresentacion/src/main/java/itson/sistemarestaurantepresentacion/comandas/BuscadorComandas.java
@@ -1,20 +1,16 @@
-package itson.sistemarestaurantepresentacion.ingredientes;
+package itson.sistemarestaurantepresentacion.comandas;
 
 import itson.sistemarestaurantedominio.Ingrediente;
 import itson.sistemarestaurantedominio.UnidadMedida;
 import itson.sistemarestaurantedominio.dtos.NuevoIngredienteDTO;
 import itson.sistemarestaurantenegocio.IIngredientesBO;
 import itson.sistemarestaurantenegocio.excepciones.NegocioException;
-import itson.sistemarestaurantepresentacion.control.Control;
 import itson.sistemarestaurantepresentacion.observers.IngredienteSeleccionadoObserver;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
-import javax.swing.event.TableModelEvent;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
@@ -24,17 +20,17 @@ import javax.swing.table.TableColumnModel;
  * @author Pedro Morales Esquer, Juan Pablo Heras Carrazco, Victoria Valenzuela
  * Soto
  */
-public class BuscadorIngredientes extends javax.swing.JFrame {
+public class BuscadorComandas extends javax.swing.JFrame {
 
     private List<IngredienteSeleccionadoObserver> observers = new ArrayList<>();
     private NuevoIngredienteDTO nuevoIngredienteDTO;
     private IIngredientesBO ingredientesBO;
-    private static final Logger LOG = Logger.getLogger(BuscadorIngredientes.class.getName());
+    private static final Logger LOG = Logger.getLogger(BuscadorComandas.class.getName());
 
     /**
      * Constructor del frame BuscadorProductos.
      */
-    public BuscadorIngredientes(IIngredientesBO ingredientesBO) {
+    public BuscadorComandas(IIngredientesBO ingredientesBO) {
         initComponents();
         this.setResizable(false);
         this.setLocationRelativeTo(null);
@@ -46,13 +42,13 @@ public class BuscadorIngredientes extends javax.swing.JFrame {
     }
 
     private void ocultarColumnaID() {
-        TableColumnModel columnModel = tablaIngredientes.getColumnModel();
+        TableColumnModel columnModel = tablaComandas.getColumnModel();
         TableColumn columnaID = columnModel.getColumn(0);
         columnModel.removeColumn(columnaID);
     }
 
     private void cargarTabla() {
-        DefaultTableModel modeloTabla = (DefaultTableModel) tablaIngredientes.getModel();
+        DefaultTableModel modeloTabla = (DefaultTableModel) tablaComandas.getModel();
         modeloTabla.setRowCount(0);
 
         String filtro = this.txtBuscar.getText();
@@ -105,9 +101,9 @@ public class BuscadorIngredientes extends javax.swing.JFrame {
     }
 
     private Long obtenerIdIngrediente() {
-        int filaSeleccionada = tablaIngredientes.getSelectedRow();
+        int filaSeleccionada = tablaComandas.getSelectedRow();
         if (filaSeleccionada != -1) {
-            return Long.valueOf(tablaIngredientes.getModel().getValueAt(filaSeleccionada, 0).toString());
+            return Long.valueOf(tablaComandas.getModel().getValueAt(filaSeleccionada, 0).toString());
         }
         return null;
     }
@@ -117,7 +113,7 @@ public class BuscadorIngredientes extends javax.swing.JFrame {
             String filtroBusqueda = this.txtBuscar.getText();
             List<Ingrediente> ingredientes = this.ingredientesBO.consultar(filtroBusqueda);
             //Este objeto permite interactuar con los elementos de la tabla
-            DefaultTableModel modeloTabla = (DefaultTableModel) this.tablaIngredientes.getModel();
+            DefaultTableModel modeloTabla = (DefaultTableModel) this.tablaComandas.getModel();
             modeloTabla.setRowCount(0);
             for (Ingrediente ingrediente : ingredientes) {
                 Object[] fila = {
@@ -146,8 +142,8 @@ public class BuscadorIngredientes extends javax.swing.JFrame {
         lblBuscar = new javax.swing.JLabel();
         txtBuscar = new javax.swing.JTextField();
         btnBuscar = new javax.swing.JButton();
-        pnlTablaIngredientes = new javax.swing.JScrollPane();
-        tablaIngredientes = new javax.swing.JTable();
+        pnlTablaComandas = new javax.swing.JScrollPane();
+        tablaComandas = new javax.swing.JTable();
         btnLimpiar = new javax.swing.JButton();
         botonSeleccionar = new javax.swing.JButton();
 
@@ -162,12 +158,12 @@ public class BuscadorIngredientes extends javax.swing.JFrame {
             }
         });
 
-        tablaIngredientes.setModel(new javax.swing.table.DefaultTableModel(
+        tablaComandas.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "ID", "Ingrediente", "Unidad de Medida", "Stock"
+                "ID", "Mesa", "Hora", "Total"
             }
         ) {
             boolean[] canEdit = new boolean [] {
@@ -178,13 +174,13 @@ public class BuscadorIngredientes extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
-        tablaIngredientes.setColumnSelectionAllowed(true);
-        pnlTablaIngredientes.setViewportView(tablaIngredientes);
-        tablaIngredientes.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-        if (tablaIngredientes.getColumnModel().getColumnCount() > 0) {
-            tablaIngredientes.getColumnModel().getColumn(0).setResizable(false);
-            tablaIngredientes.getColumnModel().getColumn(1).setResizable(false);
-            tablaIngredientes.getColumnModel().getColumn(3).setResizable(false);
+        tablaComandas.setColumnSelectionAllowed(true);
+        pnlTablaComandas.setViewportView(tablaComandas);
+        tablaComandas.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        if (tablaComandas.getColumnModel().getColumnCount() > 0) {
+            tablaComandas.getColumnModel().getColumn(0).setResizable(false);
+            tablaComandas.getColumnModel().getColumn(1).setResizable(false);
+            tablaComandas.getColumnModel().getColumn(3).setResizable(false);
         }
 
         btnLimpiar.setText("Limpiar");
@@ -208,7 +204,7 @@ public class BuscadorIngredientes extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(pnlTablaIngredientes)
+                    .addComponent(pnlTablaComandas)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(lblBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -235,7 +231,7 @@ public class BuscadorIngredientes extends javax.swing.JFrame {
                         .addComponent(btnLimpiar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(btnBuscar)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(pnlTablaIngredientes, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(pnlTablaComandas, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(botonSeleccionar)
                 .addContainerGap(16, Short.MAX_VALUE))
@@ -251,19 +247,19 @@ public class BuscadorIngredientes extends javax.swing.JFrame {
 
     private void btnLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimpiarActionPerformed
         this.txtBuscar.setText("");  // Limpiar el campo de búsqueda
-        DefaultTableModel model = (DefaultTableModel) tablaIngredientes.getModel();
+        DefaultTableModel model = (DefaultTableModel) tablaComandas.getModel();
         model.setRowCount(0);  // Limpiar las filas de la tabla
         this.cargarTabla();  // Volver a cargar la tabla con los datos originales
     }//GEN-LAST:event_btnLimpiarActionPerformed
 
     private void botonSeleccionarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonSeleccionarActionPerformed
-        int filaSeleccionada = tablaIngredientes.getSelectedRow();
+        int filaSeleccionada = tablaComandas.getSelectedRow();
         if (filaSeleccionada != -1) {
             Long id = obtenerIdIngrediente();
-            String nombre = (String) tablaIngredientes.getModel().getValueAt(filaSeleccionada, 1);
-            UnidadMedida unidadMedida = (UnidadMedida) tablaIngredientes.getModel().getValueAt(filaSeleccionada, 2);
+            String nombre = (String) tablaComandas.getModel().getValueAt(filaSeleccionada, 1);
+            UnidadMedida unidadMedida = (UnidadMedida) tablaComandas.getModel().getValueAt(filaSeleccionada, 2);
 
-            Integer stock = Integer.parseInt(tablaIngredientes.getModel().getValueAt(filaSeleccionada, 3).toString());
+            Integer stock = Integer.parseInt(tablaComandas.getModel().getValueAt(filaSeleccionada, 3).toString());
             //TipoProducto tipoProducto = TipoProducto.valueOf(tipo);
 
             NuevoIngredienteDTO nuevoIngrediente = new NuevoIngredienteDTO(id, nombre, stock, unidadMedida);
@@ -283,8 +279,8 @@ public class BuscadorIngredientes extends javax.swing.JFrame {
     private javax.swing.JButton btnBuscar;
     private javax.swing.JButton btnLimpiar;
     private javax.swing.JLabel lblBuscar;
-    private javax.swing.JScrollPane pnlTablaIngredientes;
-    private javax.swing.JTable tablaIngredientes;
+    private javax.swing.JScrollPane pnlTablaComandas;
+    private javax.swing.JTable tablaComandas;
     private javax.swing.JTextField txtBuscar;
     // End of variables declaration//GEN-END:variables
 }
